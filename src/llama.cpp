9de8a51355268b9e5e96856a59f7c99780807cb3
ws8462 <ws8462@naver.com>
2024-11-14 19:15:51 +0900

    Update llama.cpp

--- src/llama.cpp
+++ src/llama.cpp
@@ -107,8 +107,8 @@
 #define LLAMA_MAX_NODES   8192
 #define LLAMA_MAX_EXPERTS 160
 
-#define NUM_ATTN_HEAD 14
-#define NUM_KV_HEAD 7
+#define NUM_ATTN_HEAD 10
+#define NUM_KV_HEAD 1
 //
 // logging
 //
